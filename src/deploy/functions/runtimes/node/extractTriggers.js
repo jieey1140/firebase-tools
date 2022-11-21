613da0628e4d1fae026a1bc92dbf4eda7cdec010
jieey1140 <seihoonkim@hansei.kr>
2022-11-21 18:29:56 +0900

    delete new line

--- src/deploy/functions/runtimes/node/extractTriggers.js
+++ src/deploy/functions/runtimes/node/extractTriggers.js
@@ -4,7 +4,7 @@
 "use strict";
 
 /**
- * @param {Object} mod module, usually the result of require(functions/index.js)
+ * @param {object} mod module, usually the result of require(functions/index.js)
  * @param {Array<object>} triggers array of ParsedTriggerDefinition to extend (in-place).
  * @param {string=} prefix optional function name prefix, for example when using grouped functions.
  */
@@ -13,7 +13,6 @@ var extractTriggers = function (mod, triggers, prefix) {
   for (var funcName of Object.keys(mod)) {
     var child = mod[funcName];
     if (typeof child === "function" && child.__trigger && typeof child.__trigger === "object") {
-
       var trigger = {};
       for (var key of Object.keys(child.__trigger)) {
         trigger[key] = child.__trigger[key];
