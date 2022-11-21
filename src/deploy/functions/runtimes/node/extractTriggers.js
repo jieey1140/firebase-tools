ad521894f6b971d926c45f35edd08fbde85857d4
jieey1140 <seihoonkim@hansei.kr>
2022-11-21 18:13:49 +0900

    Function names cannot contain dashes. Error

--- src/deploy/functions/runtimes/node/extractTriggers.js
+++ src/deploy/functions/runtimes/node/extractTriggers.js
@@ -13,11 +13,6 @@ var extractTriggers = function (mod, triggers, prefix) {
   for (var funcName of Object.keys(mod)) {
     var child = mod[funcName];
     if (typeof child === "function" && child.__trigger && typeof child.__trigger === "object") {
-      if (funcName.indexOf("-") >= 0) {
-        throw new Error(
-          'Function name "' + funcName + '" is invalid. Function names cannot contain dashes.'
-        );
-      }
 
       var trigger = {};
       for (var key of Object.keys(child.__trigger)) {
